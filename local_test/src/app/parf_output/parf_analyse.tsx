b4f09531eb0bb2b9cfbe2e2c8a39de2d62396ff8
wzyb-52 <1534511935@qq.com>
2024-11-16 18:54:22 +0800

    modify the displaying

--- local_test/src/app/parf_output/parf_analyse.tsx
+++ local_test/src/app/parf_output/parf_analyse.tsx
@@ -6,7 +6,7 @@ import ResultComparation  from './visual/result_comparation'
 
 const Parf_analyse = () => {
     return (
-        <div className= {styles.parfInputContainer} style = {{ minWidth:'20vw'}}>
+        <div className= {styles.parfInputContainer} style = {{ maxWidth:'30vw'}}>
         {/* <div className={styles.displayMonitor}  style = {{maxHeight:'80vh',overflow:'auto'}}> */}
         <div className={styles.displayMonitor}>
           <h3>右上显示区</h3>
